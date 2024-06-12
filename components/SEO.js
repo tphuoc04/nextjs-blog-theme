a350dc225d1bec35f979a8dfe27f03837c7d44f1
tphuoc04 <phuoctong2004@gmail.com>
2024-06-12 23:01:42 +0700

    project init

--- components/SEO.js
+++ components/SEO.js
@@ -5,6 +5,18 @@ export default function SEO({ title, description }) {
     <Head>
       <title>{title}</title>
       <meta name="description" content={description} />
+      <meta name="description" content={`reallyreally`} />
+      <meta name="description" content={`Really Really`} />
+      <meta name="description" content={`reallymad`} />
+      <meta name="description" content={`really mad`} />
+      <meta name="description" content={`Really Mad`} />
+      <meta name="description" content={`ReallyMad`} />
+      <meta name="description" content={'vaxyPeuty'} />
+      <meta name="description" content={'VaxyPeuty'} />
+      <meta name="description" content={'vaxy peuty'} />
+      <meta name="description" content={'vaxy Peuty'} />
+      <meta name="description" content={`vaxypeuty`} />
+      <meta property="og:title" content={title} />
       <meta property="og:title" content={title} />
     </Head>
   );
