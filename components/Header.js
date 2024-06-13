a9639f711007953c13c47aef828b26d9a83e66e3
tphuoc04 <phuoctong2004@gmail.com>
2024-06-13 19:25:09 +0700

    Really logo

--- components/Header.js
+++ components/Header.js
@@ -4,10 +4,9 @@ import Image from 'next/image'
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <Image className='w-20 h-20 mx-auto rounded-full' src='/images/reallymad.png' alt='Profile Picture' />
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
-          {name}
+          <Image className='w-100 h-100 mx-auto rounded-full' src='/images/reallymad.png' alt='Profile Picture' />
         </Link>
       </p>
     </header>
